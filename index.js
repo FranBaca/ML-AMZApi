c49deb339593ef2a653a2e745adb55efbde8cecf
Francisco Baca <franbaca13@gmail.com>
2025-03-31 23:05:05 -0300

    port changed

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ dotenv.config()
 app.use(cors());
 app.use(express.json())
 
-const PORT = process.env.PORT | 5000;
+const PORT = process.env.PORT | 3000;
 
 const CLIENT_ID = process.env.ML_CLIENT_ID;
 const CLIENT_SECRET = process.env.ML_CLIENT_SECRET;
